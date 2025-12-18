5191c97104644fa71980b4c1a8408e3db8e1970b
Motolani <alebiosumotolani@gmail.com>
2025-12-18 21:09:26 +0100

    added pagination and django filter to settings

--- Backend/quizme_api/settings.py
+++ Backend/quizme_api/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'corsheaders',
+    'django_filters',
     
 ]
 
@@ -71,6 +72,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 SIMPLE_JWT = {
